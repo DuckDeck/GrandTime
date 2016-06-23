63d80898e157a9502a600f633a69c45bfe168d9e
Stan <StanHu@HuStans-MacBook-Pro.local>
2016-06-23 08:45:15 +0800

    Init the project

---
+++ GrandTimeDemo/GrandTime/GrandTime.swift
@@ -0,0 +1,13 @@
+//
+//  GrandTime.swift
+//  GrandTimeDemo
+//
+//  Created by HuStan on 6/23/16.
+//  Copyright © 2016 StanHu. All rights reserved.
+//
+
+import UIKit
+
+class GrandTime: NSObject {
+
+}
