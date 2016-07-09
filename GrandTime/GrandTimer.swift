7aeabc423cf027079f1aa94e08d100eabdeb481c
DuckDeck <StanHu@HuStans-MacBook-Pro.local>
2016-07-09 14:23:23 +0800

    add timer feature

--- GrandTime/GrandTimer.swift
+++ GrandTime/GrandTimer.swift
@@ -10,4 +10,113 @@ import UIKit
 
 class GrandTimer: NSObject {
     
+    
+    struct timerFlags {
+       static  var  timerIsInvalid:UInt32 = 0
+    }
+    
+    var timeSpan:TimeSpan?
+     var target:AnyObject?
+    var selector:Selector?
+   public var userInfo:AnyObject?
+    var repeats:Bool = false
+    var privateSerialQueue:dispatch_queue_t?
+    var timer:dispatch_source_t?
+    
+   private override init() {
+        super.init()
+    }
+    
+   public convenience init(timespan:TimeSpan,target:AnyObject,sel:Selector,userInfo:AnyObject?,repeats:Bool,dispatchQueue:dispatch_queue_t){
+        self.init()
+        self.timeSpan = timespan
+        self.target = target
+        self.selector = sel
+        self.userInfo = userInfo
+        self.repeats = repeats
+        let privateQueueName = "grandTime\(self)"
+        self.privateSerialQueue = dispatch_queue_create(privateQueueName.cStringUsingEncoding(NSUTF8StringEncoding)!, DISPATCH_QUEUE_SERIAL)
+        dispatch_set_target_queue(self.privateSerialQueue, dispatchQueue)
+        self.timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, self.privateSerialQueue)
+    }
+    
+  public  static func scheduleTimerWithTimeSpan(timespan:TimeSpan,target:AnyObject,sel:Selector,userInfo:AnyObject?,repeats:Bool,dispatchQueue:dispatch_queue_t)->GrandTimer{
+        let timer = GrandTimer(timespan: timespan, target: target, sel: sel, userInfo: userInfo, repeats: repeats, dispatchQueue: dispatchQueue)
+        timer.schedule()
+        return timer
+    
+    }
+    
+    
+    
+  public  func schedule() {
+        resetTimerProperties()
+        weak var weakSelf = self
+        dispatch_source_set_event_handler(self.timer!) { 
+            weakSelf?.timerFired()
+        }
+        dispatch_resume(self.timer!)
+    }
+    
+    deinit{
+        invalidate()
+    }
+    
+  public  func fire() {
+        timerFired()
+    }
+    
+    
+    
+  public  func invalidate() {
+        if OSAtomicTestAndSetBarrier(7, &timerFlags.timerIsInvalid) {
+            if  let timer = self.timer{
+                dispatch_async(self.privateSerialQueue!, {
+                    dispatch_source_cancel(timer)
+                })
+            }
+        }
+    }
+    
+    func timerFired() {
+        if OSAtomicAnd32OrigBarrier(1, &timerFlags.timerIsInvalid) > 0{
+            return
+        }
+        self.target?.performSelector(self.selector!, withObject: self)
+        if !self.repeats {
+            self.invalidate()
+        }
+        
+    }
+    var _tolerance:TimeSpan = TimeSpan()
+   public var tolerance:TimeSpan?{
+        set{
+            objc_sync_enter(self)
+            if newValue != nil && _tolerance != newValue{
+                _tolerance = newValue!
+            }
+            resetTimerProperties()
+            objc_sync_exit(self)
+
+        }
+        get{
+//            objc_sync_enter(self)
+            return _tolerance
+//            objc_sync_exit(self)
+        }
+    }
+    
+    func resetTimerProperties()  {
+        let intervalInNanoseconds:Int64 = Int64(self.timeSpan!.milliseconds) * 1000000
+        let toleranceInNanoseconds:UInt64 = UInt64(self.tolerance!.milliseconds) * 1000000
+        dispatch_source_set_timer(self.timer!, dispatch_time(DISPATCH_TIME_NOW, intervalInNanoseconds), UInt64(intervalInNanoseconds), toleranceInNanoseconds)
+    }
+    
+    
+    
+    override var description: String{
+        return "timeSpan = \(timeSpan) target = \(target) selector = \(selector) userinfo = \(userInfo) repeats = \(repeats) timer= \(timer)"
+    }
+    
+    
 }
---
+++ GrandTimeDemo/GrandTimeDemo/TimerViewController.swift
@@ -0,0 +1,52 @@
+//
+//  TimerViewController.swift
+//  GrandTimeDemo
+//
+//  Created by HuStan on 7/8/16.
+//  Copyright © 2016 StanHu. All rights reserved.
+//
+
+import UIKit
+
+class TimerViewController: UIViewController {
+
+    @IBOutlet weak var lblTimer: UILabel!
+    var timer:GrandTimer?
+    var seco = 0
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let dispatch = dispatch_queue_create("test", DISPATCH_QUEUE_CONCURRENT)
+        timer = GrandTimer.scheduleTimerWithTimeSpan(TimeSpan.fromSeconds(1), target: self, sel: "tick", userInfo: nil, repeats: true, dispatchQueue: dispatch)
+        timer?.fire()
+        // Do any additional setup after loading the view.
+    }
+    
+    func tick()  {
+        seco = seco + 1
+        dispatch_async(dispatch_get_main_queue()) { 
+                self.lblTimer.text = "\(self.seco)"
+        }
+    
+    }
+
+    deinit{
+        print("\(self.dynamicType)) the view deinit which means the timer release the viewcontrollre")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
